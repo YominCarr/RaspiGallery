ef5608d89c668c4bda3d61bed2a5d10db907c76b
YominCarr <alex.arth@googlemail.com>
2018-12-28 19:18:29 +0100

    Also compute width available per image with a reasonable estimation for space occupied by margins / paddings.

--- css/GalleryComputed.php
+++ css/GalleryComputed.php
@@ -8,5 +8,15 @@ header("Content-type: text/css; charset: UTF-8");
 
 .thumbnailColumn .emptyDummyImage {
     width: <?php echo Config::thumbnailMaxWidth ?>px;
-height: <?php echo Config::thumbnailMaxHeight ?>px;
+    height: <?php echo Config::thumbnailMaxHeight ?>px;
+}
+
+<?php
+// 0.9 to account for margins / paddings as an estimation
+$widthPerPic = (100.0 / Config::numberImagesPerRow) * 0.9;
+?>
+
+.thumbnailColumn {
+    min-width: <?php echo $widthPerPic; ?>%;
+    max-width: <?php echo $widthPerPic; ?>%;
 }
